76a7f84475762b74ac3886c29e4fcd069d28aa93
Sudheer-999 <goddusudheer@gmail.com>
2023-05-22 06:43:09 +0000

    demo

---
+++ src/components/DemoProject/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+const Demo = () => (
+  <div>
+    <h1>Demo Project</h1>
+    <img
+      src="https://res.cloudinary.com/djzsbpran/image/upload/v1683614611/cld-sample-4.jpg"
+      alt="nope"
+      className="pic"
+    />
+    <p>This is the demo project</p>
+  </div>
+);
+
+export default Demo;
